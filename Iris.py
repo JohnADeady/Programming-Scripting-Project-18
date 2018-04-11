375dc88ccbab859e643de322df8a90a3b0055bba
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-04-11 21:34:59 +0100

    Update Iris.py
    Updated comments

--- Iris.py
+++ Iris.py
@@ -1,10 +1,11 @@
 # John Deady, 2018-04-08
 # Analysis of the  Iris Data Set
+# Downloading the appropriate the libaries for this analysis
 
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-sns.set(color_codes=True)
+sns.set(color_codes=True)       # Color codes can be set through the high-level seaborn style manager.
 import pandas as pd
 
 # Read the file into the array
