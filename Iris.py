537411a489408d0b39e11c56408e93f4690338f8
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-04-25 20:49:41 +0100

    Update Iris.py
    Finalised  data set

--- Iris.py
+++ Iris.py
@@ -1,15 +1,18 @@
 # John Deady 2018-04-13
 # Analysis of the Iris Dataset
 
-# Import the appropriate Libraries needed for this analysis
+
+# Import the appropriate libraries needed for this analysis
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set(color_codes=True)
 import pandas as pd
 import pylab
+from mpl_toolkits.mplot3d import Axes3D
 
-# Read data file into array
+
+# Download Iris Data set. Adapted from http://archive.ics.uci.edu/ml/datasets/Iris
 data = pd.read_csv('project/Iris.data.csv')
 print (data.head(5))   # Prints the top 5 lines the the data
 print (data.tail(5))   # Prints the bottom 5 lines of the data
@@ -18,7 +21,7 @@ print (data.tail(5))   # Prints the bottom 5 lines of the data
 species = list(data["species"].unique())
 print("Types of species: %s\n" % species)
 
-# The lenght of data to be analysis 
+# Print the lenght of the dataset
 print("Dataset length: %i\n" % len(data))
 
 # Print the min and max for each category
@@ -27,7 +30,7 @@ print("Sepal width range:  [%s, %s]" % (min(data["sepal_width"]), max(data["sepa
 print("Petal length range: [%s, %s]" % (min(data["petal_length"]), max(data["petal_length"])))
 print("Petal width range:  [%s, %s]\n" % (min(data["petal_width"]), max(data["petal_width"])))
 
-#Prin the mean of each category
+# Print the mean of each category
 print("Sepal length mean:\t %f" % np.mean(data["sepal_length"]))
 print("Sepal width mean: \t %f" % np.mean(data["sepal_width"]))
 print("Petal length mean:\t %f" % np.mean(data["petal_length"]))
@@ -39,18 +42,17 @@ print("Sepal width stddev: \t %f" % np.std(data["sepal_width"]))
 print("Petal length stddev:\t %f" % np.std(data["petal_length"]))
 print("Petal width stddev: \t %f\n" % np.std(data["petal_width"]))
 
-# Print data in table format
+# Print dataset in table format beginning at 2nd column usig panda
 print("Data describe\n---")
 print(data[data.columns[1:]].describe())
 
-# Histogram for each category and show results
+# Histogram for each category and show the histogram
 data.hist(
-    column=["sepal_length", "sepal_width", "petal_length", "petal_width"], figsize=(10, 10))
+    column=["sepal_length", "sepal_width", "petal_length", "petal_width"], figsize=(10, 6))
 pylab.suptitle("Analyzing distribution for the series", fontsize="xx-large")
 plt.show("Analysis of Iris Dataset")
 
-
-# Scatter Graph 
+# Scatter plot shows the ratio between the width and length of sepal using seaborn
 iris = sns.load_dataset("iris")
 iris["ID"] = iris.index
 iris["ratio"] = iris["sepal_length"]/iris["sepal_width"]
@@ -60,8 +62,7 @@ sns.lmplot(x="ID", y="ratio", data=iris, hue="species", fit_reg=False, legend=Fa
 plt.legend()
 plt.show()
 
-
-# Scatter Graph 
+# Scatter plot shows the ratio between the width and length of sepal using seaborn 
 iris = sns.load_dataset("iris")
 iris["ID"] = iris.index
 iris["ratio"] = iris["petal_length"]/iris["petal_width"]
@@ -69,9 +70,9 @@ iris["ratio"] = iris["petal_length"]/iris["petal_width"]
 sns.lmplot(x="ID", y="ratio", data=iris, hue="species", fit_reg=False, legend=False)
 
 plt.legend()
-plt.show()
+plt.show()  
 
-# 3D
+# 3D lot to show the correlation of the iris data set
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 X = [data["petal_width"], data["petal_length"]]
@@ -92,6 +93,15 @@ plt.show()
 sns.pairplot(data, hue="species")
 plt.show()
 
-# violinplots on petal-length for each species
+# violinplots for each category
 sns.violinplot(data=data,x="species", y="petal_length")
+plt.show()
+
+sns.violinplot(data=data,x="species", y="petal_width")
+plt.show()
+
+sns.violinplot(data=data,x="species", y="sepal_length")
+plt.show()
+
+sns.violinplot(data=data,x="species", y="sepal_width")
 plt.show()
